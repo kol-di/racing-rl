31bac51af27eb59895683a68eb47013f6d4cd7c1
Dmitry Kolobaev <kol-di@yandex.ru>
2024-11-29 22:58:14 +0300

    weight init

--- racingrl/src/net.py
+++ racingrl/src/net.py
@@ -1,6 +1,7 @@
 import torch.nn as nn
 import torch
 import numpy as np
+from itertools import chain
 
 
 class A2CNet(nn.Module):
@@ -38,9 +39,31 @@ class A2CNet(nn.Module):
             nn.Linear(512, 1)
         )
 
+        self._init_weights()
+
+
     def _get_conv_out(self, shape):
         o = self.conv_embedding(torch.zeros(1, *shape))
         return int(np.prod(o.size()))
+    
+    def _init_weights(self):
+        modules_for_kaiming = chain(
+            self.conv_embedding.modules(), 
+            self.policy_var.modules(), 
+            self.value.modules()
+        )
+        modules_for_xavier = chain(
+            self.policy_mu.modules()
+        )
+
+        for m in modules_for_kaiming:
+            if isinstance(m, nn.Linear):
+                torch.nn.init.kaiming_normal_(m.weight)
+
+        for m in modules_for_xavier:
+            if isinstance(m, nn.Linear):
+                torch.nn.init.xavier_normal_(m.weight)
+
 
     def forward(self, x):
         conv_out = self.conv_embedding(x)
